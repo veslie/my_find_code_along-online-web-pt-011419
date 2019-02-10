020042580336dc7563cc3d08db63a21aff938314
veslie losius <veslielosius@yahoo.com>
2019-02-10 19:31:50 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-i = 0 
- while i < collection.length
- yiel(collection[i]) 
- i += 1 
- end 
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
 end
